6392ee970d07b8e196fd0b94a1c41b44010bce79
FsNeru <lucastecera@gmail.com>
2022-12-14 15:12:24 -0300

    Ejercicio Finalizado

---
+++ OpenbootcampJBEjercicioTema1/src/com/openbootcamp/ejercicio1/Main.java
@@ -0,0 +1,33 @@
+package com.openbootcamp.ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nombre = "FsNeru";
+        System.out.println("Mi nombre es " + nombre);
+        char primerLetraNombre = 'F';
+        System.out.println("La primera letra de mi nombre es " + primerLetraNombre);
+
+        long dni = 12345678;
+        System.out.println("Mi DNI es " + dni);
+        int edad = 18;
+        System.out.println("Mi edad es " + edad +" años");
+        byte numFavorito = 7;
+        System.out.println("Mi numero favorito es el " + numFavorito);
+        short numMascotas = 6;
+        System.out.println("Tengo " + numMascotas + " mascotas");
+
+
+        float promedioSecundaria = 7.5f;
+        System.out.println("Mi promedio de secundaria fue " + promedioSecundaria);
+        double promedioFacultad = 6.33333333333d;
+        System.out.println("Mi promedio de facultad fue " + promedioFacultad);
+
+        boolean conoceJava = true;
+        System.out.println("Conoce de java?:  " + conoceJava );
+        boolean conoceC = false;
+        System.out.println("Conoce de C?:  " + conoceC );
+
+
+    }
+}
